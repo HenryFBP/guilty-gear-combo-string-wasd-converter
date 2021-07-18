4266fb8f802eff8a6df00a318a0df688b1e4aec8
Henry Post <henryfbp@gmail.com>
2021-07-18 14:41:53 -0500

    save content when window closes

--- src/window.js
+++ src/window.js
@@ -1,24 +1,69 @@
+DEFAULT_COMBOSTRING = '5K > 6S > 236K';
+
 function selection() {
     return window.getSelection().toString()
 }
 
+function getStoredComboString() {
+    return chrome.storage.sync.get('combostring').value
+}
+
+function getComboString() {
+    return this.document.getElementById('inputString').value;
+}
+
+function setComboString(s) {
+    this.document.getElementById('inputString').value = s;
+}
+
+function setConvertedComboString(s) {
+    document.getElementById('output').innerText = s;
+}
+
 function doConvert() {
-    let e = document.getElementById('outputString');
-    let converted = convert_combostring(document.getElementById('inputString').value)
+    let converted = convert_combostring(getComboString());
     console.log(converted);
 
-    document.getElementById('output').innerText = converted;
+    setConvertedComboString(converted);
 }
 
 window.addEventListener('load', function () {
 
+    // when "input data" changes
     this.document.getElementById('inputString').addEventListener('input', function (e) {
-        doConvert();
+        doConvert(); //convert
+
+        //store current input string value 
+        chrome.storage.sync.set({ "combostring": getComboString() }, function(result){
+            console.log('changed text');
+            console.log(result);
+        });
+
     })
 
-    //show example
-    this.document.getElementById('inputString').value = '5K > 6S > 236K';
-    doConvert();
+    //show example combo string if data not set
+    chrome.storage.sync.get('combostring', function (result) {
+        console.log('Value retrieved from storage is ' + result.combostring);
+
+        // if we don't have one stored, set default
+        if (!result.combostring) {
+
+            setComboString(DEFAULT_COMBOSTRING);
+            chrome.storage.sync.set({ "combostring": DEFAULT_COMBOSTRING }, 
+                function (result) { });
+        } else {
+            console.log('non-default value from storage')
+            setComboString(result.combostring);
+        }
+
+        //convert regardless
+        doConvert(); //convert
+
+    });
+
+
+doConvert();
+
 
 });
 // let cs = "5K > 6S > 236K";
