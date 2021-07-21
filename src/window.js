ab20ad4271b15a91382d5231a53266a09fbdba05
Henry Post <henryfbp@gmail.com>
2021-07-21 12:15:33 -0500

    use localstorage instead of chrome storage

--- src/window.js
+++ src/window.js
@@ -1,13 +1,9 @@
 DEFAULT_COMBOSTRING = '5K > 6S > 236K';
 
-DEFAULT_KEYMAP= new DefaultWASDButtonMap();
+DEFAULT_KEYMAP = new DefaultWASDButtonMap();
 
 function selection() {
     return window.getSelection().toString()
-}
-
-function getStoredComboString() {
-    return chrome.storage.sync.get('combostring').value
 }
 
 function getComboString() {
@@ -35,48 +31,19 @@ window.addEventListener('load', function () {
     this.document.getElementById('inputString').addEventListener('input', function (e) {
         doConvert(); //convert
 
-        //store current input string value 
-        chrome.storage.sync.set({ "combostring": getComboString() }, function(result){
-            console.log('changed text');
-            console.log(result);
-        });
-
+        localStorage['comboString'] = getComboString();
     })
 
-    //show example combo string if data not set
-    chrome.storage.sync.get('combostring', function (result) {
-        console.log('Value retrieved from storage is ' + result.combostring);
+    // if we don't have one stored, set default
+    if (!localStorage['comboString']) {
+        setComboString(DEFAULT_COMBOSTRING);
+        localStorage['comboString'] = DEFAULT_COMBOSTRING;
+    } else {
+        console.log('non-default value from storage');
+        setComboString(localStorage['comboString']);
+    }
 
-        // if we don't have one stored, set default
-        if (!result.combostring) {
-
-            setComboString(DEFAULT_COMBOSTRING);
-            chrome.storage.sync.set({ "combostring": DEFAULT_COMBOSTRING }, 
-                function (result) { });
-        } else {
-            console.log('non-default value from storage')
-            setComboString(result.combostring);
-        }
-
-        //convert regardless
-        doConvert(); //convert
-
-    });
-
-
-doConvert();
-
+    //convert regardless
+    doConvert(); //convert
 
 });
-// let cs = "5K > 6S > 236K";
-
-// console.log("hello chrome :3");
-// console.log(cs);
-// console.log(convert_combostring(cs));
-
-
-// setTimeout(function(){ 
-
-//     console.log(convert_combostring(selection()));
-
-// }, 300);
